bc9e334a7c3d91edf61f129d2e00639acd024092
vallenov <a2lenov@mail.ru>
2022-05-10 16:27:04 +0300

    Косметические правки

--- TLL.py
+++ TLL.py
@@ -1,11 +1,11 @@
 import pygame
 import random
 
-from TLLclasses import constants, LL, Food, Smell, Wall
+from TLLclasses import Constants, LL, Food, Smell, Wall
 
 pygame.init()
 pygame.mixer.init()
-screen = pygame.display.set_mode((constants.WIDTH.value, constants.HEIGHT.value))
+screen = pygame.display.set_mode((Constants.WIDTH.value, Constants.HEIGHT.value))
 pygame.display.set_caption("TheLittleLife")
 clock = pygame.time.Clock()
 
@@ -18,33 +18,34 @@ walls = pygame.sprite.Group()
 LittleLife = LL()
 all_sprites.add(LittleLife)
 
+
 def walls_generate():
     for _ in range(random.randint(20, 100)):
         w = Wall()
         all_sprites.add(w)
         walls.add(w)
 
+
 def new_food():
     f = Food()
-    s = Smell(f.rect.centerx, f.rect.bottom, f.size)
     all_sprites.add(f)
     food.add(f)
-    all_sprites.add(s)
-    food.add(s)
+
 
 def draw_text(surf, text, size, x, y):
     font = pygame.font.Font(font_name, size)
-    text_surface = font.render(text, True, constants.WHITE.value)
+    text_surface = font.render(text, True, Constants.WHITE.value)
     text_rect = text_surface.get_rect()
     text_rect.midtop = (x, y)
     surf.blit(text_surface, text_rect)
+
 
 walls_generate()
 
 running = True
 while running:
     # Держим цикл на правильной скорости
-    clock.tick(constants.FPS.value)
+    clock.tick(Constants.FPS.value)
     # Ввод процесса (события)
     for event in pygame.event.get():
         # check for closing window
@@ -57,7 +58,7 @@ while running:
     all_sprites.update()
 
     # Рендеринг
-    screen.fill(constants.GREEN.value)
+    screen.fill(Constants.GREEN.value)
     all_sprites.draw(screen)
 
     #draw_text(screen, 'LL', 10, LittleLife.rect.centerx, LittleLife.rect.bottom - LittleLife.size-2)
