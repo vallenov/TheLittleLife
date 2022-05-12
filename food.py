468ca781084addb248f4a6a206ecaca3dfe6c600
vallenov <a2lenov@mail.ru>
2022-05-13 00:55:31 +0300

    Добавлена файл food.py

---
+++ food.py
@@ -0,0 +1,14 @@
+import pygame
+import random
+from TLLclasses import GObject, Constants
+
+
+class Food(GObject):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.size = 5
+        self.image = pygame.Surface((self.size, self.size))
+        self.image.fill(Constants.BLACK.value)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = random.randint(2, Constants.WIDTH.value - 1)
+        self.rect.bottom = random.randint(2, Constants.HEIGHT.value - 1)
