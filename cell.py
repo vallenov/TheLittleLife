86c8e28b6dd331e29321bb4c6a0fd8b136b9336b
vallenov <a2lenov@mail.ru>
2022-05-15 23:58:11 +0300

    Доработан алгоритм поиска еды

--- cell.py
+++ cell.py
@@ -50,10 +50,88 @@ class Cell(GObject):
             self.born()
             self.energy -= 700
 
-# 00 01 02 03
-# 10 11 12 13
-# 20 21 22 23
-# 30 31 32 33
+# 00 01 02 03 04
+# 10 11 12 13 14
+# 20 21 22 23 24
+# 30 31 32 33 34
+# 40 41 42 43 44
+
+# 00 10 20 30 40
+# 01 11 21 31 41
+# 02 12 22 32 42
+# 03 13 23 33 43
+# 04 14 24 34 44
+
+    # def next_step(self, goal):
+    #     # self.speedx = 0
+    #     # self.speedy = 0
+    #     if -self.size < self.rect.x - goal.rect.x < self.size:
+    #         print('x ==')
+    #         self.speedx = +self.size if self.rect.x - goal.rect.x <= 0 else -self.size
+    #         self.speedy = 0
+    #         # return
+    #     elif self.rect.x - goal.rect.x < -self.size:
+    #         print('x <')
+    #         self.speedx = +self.size
+    #         return
+    #     elif self.rect.x - goal.rect.x > self.size:
+    #         print('x <')
+    #         self.speedx = -self.size
+    #         return
+    #     if -self.size < self.rect.y - goal.rect.y < self.size:
+    #         print('y ==')
+    #         self.speedy = +self.size if self.rect.y - goal.rect.y <= 0 else -self.size
+    #         self.speedx = 0
+    #         # return
+    #     elif self.rect.y - goal.rect.y < -self.size:
+    #         print('y <')
+    #         self.speedy = +self.size
+    #         return
+    #     elif self.rect.y - goal.rect.y > self.size:
+    #         print('y >')
+    #         self.speedy = -self.size
+
+    def next_step(self, goal):
+        self.speedx = 0
+        self.speedy = 0
+        dex = self.rect.centerx - goal.rect.centerx
+        if abs(dex) < self.size / 2:
+            if dex > 0:
+                self.speedx = -self.size
+                self.speedy = 0
+            else:
+                self.speedx = +self.size
+                self.speedy = 0
+            return
+        elif abs(dex) <= self.size:
+            if dex > 0:
+                self.speedx = -self.size
+            else:
+                self.speedx = +self.size
+        elif abs(dex) >= self.size:
+            if dex > 0:
+                self.speedx = -self.size
+            else:
+                self.speedx = +self.size
+        dey = self.rect.centery - goal.rect.centery
+        if abs(dey) < self.size / 2:
+            if dey > 0:
+                self.speedx = 0
+                self.speedy = -self.size
+            else:
+                self.speedx = 0
+                self.speedy = +self.size
+            return
+        elif abs(dey) <= self.size:
+            if dey > 0:
+                self.speedy = -self.size
+            else:
+                self.speedy = +self.size
+        elif abs(dey) >= self.size:
+            if dey > 0:
+                self.speedy = -self.size
+            else:
+                self.speedy = +self.size
 
     def update(self):
         self.energy -= 1
@@ -65,31 +143,24 @@ class Cell(GObject):
                     self.goal = food if self.goal is None else self.goal
                     print((self.rect.x, self.rect.y), (self.goal.rect.x, self.goal.rect.y))
                     print((self.rect.x - self.goal.rect.x, self.rect.y - self.goal.rect.y))
+        elif not self.sight.rect.colliderect(self.goal.rect):
+            print("i've lost foood")
+            self.goal = None
         else:
-            print((self.rect.x, self.rect.y), (self.goal.rect.x, self.goal.rect.y))
-            print((self.rect.x - self.goal.rect.x, self.rect.y - self.goal.rect.y))
-            if self.rect.colliderect(self.goal.rect):
-                self.eat(self.goal, 100)
-            if -(self.size / 2) <= self.rect.x - self.goal.rect.x <= self.size / 2:
-                self.speedx = 0 if self.speedy else self.speedx
-            elif self.rect.x - self.goal.rect.x <= -(self.size / 2):
-                self.speedx = self.size
-                self.speedx = +self.speedx
-            elif self.rect.x - self.goal.rect.x >= self.size / 2:
-                self.speedx = self.size
-                self.speedx = -self.speedx
-            if -(self.size / 2) <= self.rect.y - self.goal.rect.y <= self.size / 2:
-                self.speedy = 0 if self.speedx else self.speedy
-            elif self.rect.y - self.goal.rect.y <= -(self.size / 2):
-                self.speedy = self.size
-                self.speedy = +self.speedy
-            elif self.rect.y - self.goal.rect.y >= self.size / 2:
-                self.speedy = self.size
-                self.speedy = -self.speedy
+            try:
+                if self.rect.colliderect(self.goal.rect):
+                    self.eat(self.goal, 100)
+                self.next_step(self.goal)
+                print(self.speedx, self.speedy)
+                print((self.rect.x, self.rect.y), (self.goal.rect.x, self.goal.rect.y))
+                print((self.rect.x - self.goal.rect.x, self.rect.y - self.goal.rect.y))
+            except AttributeError:
+                self.goal = None
+                pass
         self.speedx = -self.speedx if Constants.WIDTH.value <= self.rect.x or self.rect.x <= 0 else self.speedx
         self.speedy = -self.speedy if Constants.HEIGHT.value <= self.rect.y or self.rect.y <= 0 else self.speedy
-        self.rect.x += self.speedx
-        self.rect.y += self.speedy
+        self.rect.centerx += self.speedx
+        self.rect.centery += self.speedy
         self.text.update(str(self.energy // 10), (self.rect.x + 10, self.rect.y - 20))
         self.sight.update(self.rect.x - ((self.sight.size / 2) - (self.size / 2)),
                           self.rect.y - ((self.sight.size / 2) - (self.size / 2)))
