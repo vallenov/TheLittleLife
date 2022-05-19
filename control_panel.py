3bdb87ce30d8fbfeba70c7b264ed8f2800de3f6b
vallenov <a2lenov@mail.ru>
2022-05-20 01:43:08 +0300

    Добавлено отображение нескольких значений переменных

--- control_panel.py
+++ control_panel.py
@@ -1,5 +1,6 @@
 import pygame
 from GObject import GObject, Constants
+from text import Text
 
 
 class ControlPanel(GObject):
@@ -12,4 +13,32 @@ class ControlPanel(GObject):
         self.rect = self.image.get_rect()
         self.rect.right = Constants.WIDTH.value
         self.rect.bottom = Constants.HEIGHT.value
+        # GObject.all_objects.add(self)
         GObject.control_panel.add(self)
+
+        self.fps = Text(30)
+        self.current_food = Text(30)
+        self.total_food = Text(30)
+        self.current_population = Text(30)
+        self.total_born = Text(30)
+
+    def update(self):
+        pygame.display.get_surface().blit(self.image, (self.rect.x, self.rect.y))
+        self.fps.update(f'Duration: {GObject.duration // 60}m or {GObject.duration // 3600}h',
+                        (Constants.WIDTH.value - 150, Constants.HEIGHT.value - 180),
+                        Constants.BLACK.value)
+        self.fps.update(f'FPS: {GObject.fps}',
+                        (Constants.WIDTH.value - 150, Constants.HEIGHT.value - 150),
+                        Constants.BLACK.value)
+        self.current_food.update(f'Current food: {len(GObject.food)}',
+                                 (Constants.WIDTH.value - 150, Constants.HEIGHT.value - 120),
+                                 Constants.BLACK.value)
+        self.total_food.update(f'Total food: {GObject.cnt_of_food_ever}',
+                               (Constants.WIDTH.value - 150, Constants.HEIGHT.value - 90),
+                               Constants.BLACK.value)
+        self.current_population.update(f'Current population: {len(GObject.cells)}',
+                                       (Constants.WIDTH.value - 150, Constants.HEIGHT.value - 60),
+                                       Constants.BLACK.value)
+        self.total_born.update(f'Total born: {GObject.cnt_of_cells_ever}',
+                               (Constants.WIDTH.value - 150, Constants.HEIGHT.value - 30),
+                               Constants.BLACK.value)
