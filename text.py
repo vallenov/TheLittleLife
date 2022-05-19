813900d8997a9d0e309bd23c87898aec46e7b7bc
vallenov <a2lenov@mail.ru>
2022-05-20 00:21:06 +0300

    Убраны лишние действия

--- text.py
+++ text.py
@@ -1,17 +1,15 @@
 import pygame
-import random
 from GObject import GObject, Constants
 #from TLL import Game
 
 
 class Text(GObject):
-    def __init__(self, text: str, size: int, xy: tuple):
+    def __init__(self, size: int):
         pygame.sprite.Sprite.__init__(self)
         self.font = pygame.font.SysFont(pygame.font.match_font('arial'), size)
-        self.text_surface = self.font.render(text, True, Constants.WHITE.value)
-        self.text_rect = self.text_surface.get_rect()
 
     def update(self, text, xy: tuple):
-        self.text_surface = self.font.render(text, True, Constants.WHITE.value)
-        self.text_rect.midtop = xy
-        pygame.display.get_surface().blit(self.text_surface, self.text_rect)
+        text_surface = self.font.render(text, True, Constants.WHITE.value)
+        text_rect = text_surface.get_rect()
+        text_rect.midtop = xy
+        pygame.display.get_surface().blit(text_surface, text_rect)
