dc645aa12f1ca70fb724c963c21d0f58824a7801
vallenov <a2lenov@mail.ru>
2022-05-22 13:21:14 +0300

    Добавлено описание и возможность отключить компрессию списков

--- graph.py
+++ graph.py
@@ -11,34 +11,39 @@ class Graph(GObject):
         self.y_list = y
         self.size = size  # size of graph
         self.scale = 20
-        self.color = Constants.BLACK.value
+        self.color = Constants.BLACK.value  # line color
         self.part_size = self.size[0] // self.scale
         self.image = pygame.Surface(size, 5)
         self.image.fill(Constants.BLACK.value)
         self.rect = self.image.get_rect()
-        self.y_top_text = Text(20)
-        self.x_bottom_text = Text(20)
-        self.xlabel = Text(20)
-        self.ylabel = Text(20)
+        self.y_top_text = Text(20)  # print max(self.y_list) on the left-top edge
+        self.x_bottom_text = Text(20)  # print last element self.x_list on the right-bottom edge
+        self.xlabel = Text(20)  # name of x axis
+        self.ylabel = Text(20)  # name of y axis
+        self.compress = True
 
     @staticmethod
-    def compress(lst):
-        print(lst)
+    def _compress(lst):
         step = 2
         finaly_len = len(lst) // step
         i = 0
         while len(lst) > finaly_len:
             step = step if step <= len(lst[i:]) else len(lst[i:])
-            lst[i] = math.ceil(sum(lst[i:step+i]) / len(lst[i:step+i])) if max(lst) not in lst[i:step+i] else max(lst)
+            if max(lst) in lst[i:step+i]:
+                lst[i] = max(lst)
+            elif min(lst) in lst[i:step+i]:
+                lst[i] = min(lst)
+            else:
+                lst[i] = math.ceil(sum(lst[i:step+i]) / len(lst[i:step+i]))
             for _ in range(step-1):
                 lst.pop(i+1)
             i += 1
         return lst
 
     def update(self):
-        if len(self.x_list) >= 30:
-            self.x_list = self.compress(self.x_list)
-            self.y_list = self.compress(self.y_list)
+        if self.compress and len(self.x_list) >= 30:
+            self.x_list = self._compress(self.x_list)
+            self.y_list = self._compress(self.y_list)
         pygame.draw.rect(pygame.display.get_surface(), Constants.BLACK.value, self, 1)
         self.y_top_text.update(text=str(max(self.y_list)),
                                xy=(self.rect.left, self.rect.top - 15),
@@ -49,7 +54,7 @@ class Graph(GObject):
         self.ylabel.update((self.rect.centerx, self.rect.top - 15), Constants.BLACK.value)
         self.xlabel.update((self.rect.centerx, self.rect.bottom + 5), Constants.BLACK.value)
         startxy = (self.rect.left, self.rect.bottom)
-        percent = max(self.y_list)
+        percent = max(self.y_list) if max(self.y_list) > 0 else 1
         self.scale = len(self.x_list)
         self.part_size = self.size[0] / self.scale
         for part in range(self.scale):
