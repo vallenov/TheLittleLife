578054a28d2a4954ab4276bb0765a6cf902964ec
vallenov <a2lenov@mail.ru>
2022-05-27 23:40:41 +0300

    Изменено начальное количество энергии

--- cell/genotype.py
+++ cell/genotype.py
@@ -3,24 +3,6 @@ import random
 
 class Genotype:
     def __init__(self):
-        # self.size = random.randint(10, 16)
-        # self.sight_distance = random.randint(200, 300)
-        # self.anger = random.randint(0, 100)
-        # self.color = (random.randint(0, 256), random.randint(0, 256), random.randint(0, 256))
-        # self.max_energy = random.randint(800, 1500)
-        # self.born_energy = random.randint(self.max_energy - 100, self.max_energy + 300)
-        # self.birth_losses = random.randint(600, 800)
-
-        # self.dna = {
-        #     'size': self.size,
-        #     'sight_distance': self.sight_distance,
-        #     'anger': self.anger,
-        #     'color': self.color,
-        #     'max_energy': self.max_energy,
-        #     'born_energy': self.born_energy,
-        #     'birth_losses': self.birth_losses
-        # }
-
         self.dna = {
             'size': random.randint(10, 16),
             'sight_distance': random.randint(200, 300),
@@ -29,7 +11,7 @@ class Genotype:
             'max_energy': random.randint(800, 1500),
             'birth_losses': random.randint(600, 800)
         }
-        self.dna['born_energy'] = random.randint(self.dna['max_energy'] - 100, self.dna['max_energy'] + 300)
+        self.dna['energy_for_born'] = random.randint(self.dna['max_energy'] - 300, self.dna['max_energy'])
 
     def __repr__(self):
         return str(self.dna)
