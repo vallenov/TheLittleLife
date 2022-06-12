4c7066b91100c1236355cd1a6871472bd1431b83
vallenov <a2lenov@mail.ru>
2022-06-13 00:09:06 +0300

    Мелкие правки

--- graph.py
+++ graph.py
@@ -12,7 +12,7 @@ class Graph(GObject):
         self.size = size  # size of graph
         self.scale = 20
         self.color = Constants.BLACK.value  # line color
-        self.part_size = self.size[0] // self.scale
+        self.part_size = self.size[0] / self.scale
         self.image = pygame.Surface(size, 5)
         self.image.fill(Constants.BLACK.value)
         self.rect = self.image.get_rect()
@@ -49,15 +49,12 @@ class Graph(GObject):
         self.y_top_text.update(text=str(max(self.y_list)),
                                xy=(self.rect.left, self.rect.top - self.ylabel.size),
                                color=Constants.BLACK.value)
-        print(self.y_top_text.text)
         self.x_bottom_text.update(text=str(self.x_list[-1]),
                                   xy=(self.rect.right, self.rect.bottom + self.ylabel.size // 4),
                                   color=Constants.BLACK.value)
         self.ylabel.update(xy=(self.rect.left - self.ylabel.size // 2, self.rect.centery - self.ylabel.size * 2), color=Constants.BLACK.value)
         self.xlabel.update(xy=(self.rect.centerx, self.rect.bottom + 5), color=Constants.BLACK.value)
         self.scale = len(self.x_list)
-        startxy = (self.rect.left, self.rect.bottom)
-        self.part_size = self.size[0] / self.scale
         # self.y_last_value.update(text=str(self.y_list[-1]),
         #                    xy=(self.rect.right + 10, startxy[1] - 5),
         #                    color=Constants.BLACK.value)
@@ -66,6 +63,7 @@ class Graph(GObject):
         pygame.draw.rect(pygame.display.get_surface(), Constants.BLACK.value, self, 1)
         percent = max(self.y_list) if max(self.y_list) > 0 else 1
         startxy = (self.rect.left, self.rect.bottom)
+        self.part_size = self.size[0] / self.scale
         self.y_top_text.draw()
         self.x_bottom_text.draw()
         self.ylabel.draw()
