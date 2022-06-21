12b4a911eeed401020f9121749c378f9aa15188e
vallenov <a2lenov@mail.ru>
2022-06-22 00:34:39 +0300

    Добавлен класс SightDistance + метод mutation вынесен в родительский класс

--- cell/gen.py
+++ cell/gen.py
@@ -13,6 +13,24 @@ class Gen:
     def __repr__(self):
         return f'<{type(self).__name__} {self.value}>'
 
+    def mutation(self):
+        rand = random.randint(0, 100)
+        for k in self.sorted_map_keys:
+            if Size.map[k] <= rand:
+                diff = Size.map[k]
+                break
+        else:
+            diff = self.map[-1]
+        diff = random.choice([diff, -diff])
+        new_value = None
+        if self.min_value <= self.value + diff <= self.max_value:
+            new_value = self.value + diff
+        elif self.value + diff >= self.max_value:
+            new_value = self.max_value
+        elif self.value + diff <= self.min_value:
+            new_value = self.min_value
+        return new_value
+
 
 class Size(Gen):
     min_value = 10
@@ -32,21 +50,7 @@ class Size(Gen):
         self.value = self.min_value if not value else value
 
     def mutation(self):
-        rand = random.randint(0, 100)
-        for k in Size.sorted_map_keys:
-            if Size.map[k] <= rand:
-                diff = Size.map[k]
-                break
-        else:
-            print('0')
-            diff = Size.map[-1]
-        diff = random.choice([diff, -diff])
-        if self.min_value <= self.value + diff <= self.max_value:
-            return Size(self.value + diff)
-        elif self.value + diff >= self.max_value:
-            return Size(self.max_value)
-        elif self.value + diff <= self.min_value:
-            return Size(self.min_value)
+        return Size(super().mutation())
 
 
 class SightDistance(Gen):
@@ -67,22 +71,4 @@ class SightDistance(Gen):
         self.value = self.min_value if not value else value
 
     def mutation(self):
-        diff = 0
-        rand = random.randint(0, 100)
-        if 0 < rand < 75:
-            diff = 10
-        elif 75 < rand < 90:
-            diff = 20
-        elif 90 < rand < 97:
-            diff = 30
-        elif 97 < rand <= 99:
-            diff = 40
-        elif rand == 100:
-            diff = 50
-        diff = random.choice([diff, -diff])
-        if self.min_value <= self.value + diff <= self.max_value:
-            return SightDistance(self.value + diff)
-        elif self.value + diff >= self.max_value:
-            return SightDistance(self.max_value)
-        elif self.value + diff <= self.min_value:
-            return SightDistance(self.min_value)
+        return SightDistance(super().mutation())
